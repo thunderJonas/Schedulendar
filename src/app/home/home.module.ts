d3f1c25f1207f29adea86f54ebb074ffe821ef44
thunderJonas_Fischer <jf222@gmx.de>
2023-03-11 23:56:11 +0100

    alter...

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -6,14 +6,25 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { NgCalendarModule  } from 'ionic2-calendar';
+import { CalModalPageModule } from '../pages/cal-modal/cal-modal.module';
+
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe);
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgCalendarModule,
+    CalModalPageModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-DE' }
+  ]
 })
 export class HomePageModule {}
